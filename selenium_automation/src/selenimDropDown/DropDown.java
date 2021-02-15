f31ac953c1d4907149a59365fe25ca4ac2a9144a
vivek singh <viveksheat01@gmail.com>
2021-02-15 15:21:50 +0530

    add driver.close

--- selenium_automation/src/selenimDropDown/DropDown.java
+++ selenium_automation/src/selenimDropDown/DropDown.java
@@ -12,19 +12,21 @@ public class DropDown {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:\\selenium_drivers\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		
+		WebDriver driver = new ChromeDriver();
+
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		WebElement drodown=driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
-		
-		Select dp=new Select(drodown);
-		
+		WebElement drodown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+
+		Select dp = new Select(drodown);
+
 		dp.selectByVisibleText("USD");
 		System.out.println(dp.getFirstSelectedOption().getText());
 		dp.selectByIndex(2);
 		System.out.println(dp.getFirstSelectedOption().getText());
 		dp.selectByVisibleText("INR");
 		System.out.println(dp.getFirstSelectedOption().getText());
+
+			driver.close();
 	}
 
 }
